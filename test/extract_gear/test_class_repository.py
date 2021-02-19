02c26ab6f8055bb4862e9d3492a35f9f0ce6bf72
Kameron Sheppard <kameron.x.sheppard@gmail.com>
2021-02-18 19:05:05 -0500

    Refactor test class repoistory
    Ensure new arguments of known about types are handled correctly

--- test/extract_gear/test_class_repository.py
+++ test/extract_gear/test_class_repository.py
@@ -13,9 +13,29 @@ from extract_gear.stat_group_reader import StatGroupReader
 
 from train.image_scaler import ImageScaler
 
-from test.util.test_util import Arg
+from test.util.test_util import TestUtil, Arg
 
 class TestClassRepository(unittest.TestCase):
+
+  expected_instances = {
+    "api_builtin": api.api_builtin.ApiBuiltIn,
+    "api_curses": api.api_curses.ApiCurses,
+    "api_cv2": api.api_cv2.ApiCv2,
+    "api_fuzzywuzzy": api.api_fuzzywuzzy.ApiFuzzyWuzzy,
+    "api_json": api.api_json.ApiJson,
+    "api_pyautogui": api.api_pyautogui.ApiPyAutoGui,
+    "api_pytesseract": api.api_pytesseract.ApiPyTesseract,
+    "api_random": api.api_random.ApiRandom,
+    "api_tensorflow": api.api_tensorflow.ApiTensorflow,
+    "api_time": api.api_time.ApiTime,
+    "card_reader": CardReader,
+    "image_splitter": ImageSplitter,
+    "perprocess_factory": PreprocessFactory,
+    "set_type_reader": SetTypeReader,
+    "stat_group_reader": StatGroupReader,
+    "safe": bool
+  }
+
 
   @classmethod
   def setUpClass(cls):
@@ -26,90 +46,59 @@ class TestClassRepository(unittest.TestCase):
 
   def test_setTypeReader(self):
     set_type_reader = Internal2.set_type_reader()
-    self.assertEqual(type(set_type_reader.api_fuzzzywuzzy), api.api_fuzzywuzzy.ApiFuzzyWuzzy)
-    self.assertEqual(type(set_type_reader.api_pytesseract), api.api_pytesseract.ApiPyTesseract)
-    self.assertEqual(type(set_type_reader.preprocess_factory), PreprocessFactory)
+    self.assert_obj_has_attributes_of_correct_type(set_type_reader)
 
 
   def test_statGroupReader(self):
     stat_group_reader = Internal2.stat_group_reader()
-    self.assertEqual(type(stat_group_reader.api_tensorflow), api.api_tensorflow.ApiTensorflow)
-    self.assertEqual(type(stat_group_reader.preprocess_factory), PreprocessFactory)
-    self.assertEqual(type(stat_group_reader.image_scaler), ImageScaler)
+    self.assert_obj_has_attributes_of_correct_type(stat_group_reader)
 
 
   def test_cardReader(self):
     card_reader = Internal3.card_reader()
-    self.assertEqual(type(card_reader.image_splitter), ImageSplitter)
-    self.assertEqual(type(card_reader.api_builtin), api.api_builtin.ApiBuiltIn)
-    self.assertEqual(type(card_reader.api_cv2), api.api_cv2.ApiCv2)
-    self.assertEqual(type(card_reader.image_splitter), ImageSplitter)
-    self.assertEqual(type(card_reader.stat_group_reader), StatGroupReader)
-    self.assertEqual(type(card_reader.set_type_reader), SetTypeReader)
+    self.assert_obj_has_attributes_of_correct_type(card_reader)
 
 
   def test_imageSplitTask(self):
     image_split_task = TaskProvider.image_split_task()
     self.assertEqual("subtask", image_split_task.sub_task)
-    self.assertEqual(type(image_split_task.api_builtin), api.api_builtin.ApiBuiltIn)
-    self.assertEqual(type(image_split_task.api_cv2), api.api_cv2.ApiCv2)
-    self.assertEqual(type(image_split_task.image_splitter), ImageSplitter)
+    self.assert_obj_has_attributes_of_correct_type(image_split_task)
 
 
   def test_modelEvaluatorTask(self):
     model_evaluator_task = TaskProvider.model_evaluator_task()
     self.assertEqual("subtask", model_evaluator_task.sub_task)
-    self.assertEqual(type(model_evaluator_task.api_builtin), api.api_builtin.ApiBuiltIn)
-    self.assertEqual(type(model_evaluator_task.api_cv2), api.api_cv2.ApiCv2)
-    self.assertEqual(type(model_evaluator_task.api_pytesseract), api.api_pytesseract.ApiPyTesseract)
-    self.assertEqual(type(model_evaluator_task.preprocess_factory), PreprocessFactory)
-    self.assertEqual(type(model_evaluator_task.card_reader), CardReader)
+    self.assert_obj_has_attributes_of_correct_type(model_evaluator_task)
 
 
   def test_collectGearTask(self):
     collect_gear_task = TaskProvider.collect_gear_task()
-    self.assertEqual(type(collect_gear_task.api_builtin), api.api_builtin.ApiBuiltIn)
-    self.assertEqual(type(collect_gear_task.api_pyautogui), api.api_pyautogui.ApiPyAutoGui)
-    self.assertEqual(type(collect_gear_task.api_time), api.api_time.ApiTime)
+    self.assert_obj_has_attributes_of_correct_type(collect_gear_task)
 
 
   def test_indexTask(self):
     index_task = TaskProvider.index_task()
     self.assertEqual("file", index_task.file)
-    self.assertEqual(type(index_task.api_builtin), api.api_builtin.ApiBuiltIn)
-    self.assertEqual(type(index_task.api_curses), api.api_curses.ApiCurses)
-    self.assertEqual(type(index_task.api_cv2), api.api_cv2.ApiCv2)
-    self.assertEqual(type(index_task.api_json), api.api_json.ApiJson)
-    self.assertEqual(type(index_task.api_time), api.api_time.ApiTime)
+    self.assert_obj_has_attributes_of_correct_type(index_task)
 
 
   def test_trainStatValueTask(self):
     train_stat_value_task = TaskProvider.train_stat_value_task()
-    self.assertEqual(bool, type(train_stat_value_task.safe))
-    self.assertEqual(type(train_stat_value_task.api_builtin), api.api_builtin.ApiBuiltIn)
-    self.assertEqual(type(train_stat_value_task.api_cv2), api.api_cv2.ApiCv2)
-    self.assertEqual(type(train_stat_value_task.api_json), api.api_json.ApiJson)
-    self.assertEqual(type(train_stat_value_task.api_random), api.api_random.ApiRandom)
-    self.assertEqual(type(train_stat_value_task.api_tensorflow), api.api_tensorflow.ApiTensorflow)
-    self.assertEqual(type(train_stat_value_task.image_scaler), ImageScaler)
+    self.assert_obj_has_attributes_of_correct_type(train_stat_value_task)
 
 
   def test_trainStatTypeTask(self):
     train_stat_type_task = TaskProvider.train_stat_type_task()
-    self.assertEqual(bool, type(train_stat_type_task.safe))
-    self.assertEqual(type(train_stat_type_task.api_builtin), api.api_builtin.ApiBuiltIn)
-    self.assertEqual(type(train_stat_type_task.api_cv2), api.api_cv2.ApiCv2)
-    self.assertEqual(type(train_stat_type_task.api_json), api.api_json.ApiJson)
-    self.assertEqual(type(train_stat_type_task.api_random), api.api_random.ApiRandom)
-    self.assertEqual(type(train_stat_type_task.api_tensorflow), api.api_tensorflow.ApiTensorflow)
-    self.assertEqual(type(train_stat_type_task.image_scaler), ImageScaler)
+    self.assert_obj_has_attributes_of_correct_type(train_stat_type_task)
 
 
   def test_extractGear(self):
     extract_gear = TaskProvider.extract_gear()
-    self.assertEqual(type(extract_gear.api_builtin), api.api_builtin.ApiBuiltIn)
-    self.assertEqual(type(extract_gear.api_cv2), api.api_cv2.ApiCv2)
-    self.assertEqual(type(extract_gear.api_json), api.api_json.ApiJson)
-    self.assertEqual(type(extract_gear.api_pyautogui), api.api_pyautogui.ApiPyAutoGui)
-    self.assertEqual(type(extract_gear.api_time), api.api_time.ApiTime)
-    self.assertEqual(type(extract_gear.card_reader), CardReader)
+    self.assert_obj_has_attributes_of_correct_type(extract_gear)
+
+
+  def assert_obj_has_attributes_of_correct_type(self, obj):
+    attributes = TestUtil.get_class_attributes(obj)
+    for attribute_name, attribute_value in attributes:
+      if attribute_name in TestClassRepository.expected_instances:
+        self.assertEqual(type(attribute_value), TestClassRepository.expected_instances[attribute_name])
